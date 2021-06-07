4d0349391bc606352943e5e9eeef7c0ebcaabf1d
andrey.soskov <andrey.soskov@exness.com>
2021-06-07 18:56:56 +0300

    readme

--- conftest.py
+++ conftest.py
@@ -1,6 +1,16 @@
 import pytest
-
+from slugify import slugify
+from pathlib import Path
 
 #@pytest.fixture(scope="session")
 #def browser_type(playwright, browser_name: str):
     #return getattr(playwright, 'chromium')
+
+
+def pytest_runtest_makereport(item, call) -> None:
+    if call.when == "call":
+        if call.excinfo is not None and "page" in item.funcargs:
+            page = item.funcargs["page"]
+            screenshot_dir = Path(".playwright-screenshots")
+            screenshot_dir.mkdir(exist_ok=True)
+            page.screenshot(path=str(screenshot_dir / f"{slugify(item.nodeid)}.png"))
--- step_definition/demo.py
+++ step_definition/demo.py
@@ -4,5 +4,4 @@ from pytest_bdd import scenarios, given, when, then
 @given("I open browser")
 def step_impl(page):
     page.goto("https://example.com")
-    assert True
-    return
+    assert False
